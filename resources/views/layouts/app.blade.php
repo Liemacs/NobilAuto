5949a749734b418df9784217fbc32a86cf6ab002
Liemacs <ciubari@mail.ru>
2023-12-09 13:09:02 +0200

    first coomit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <h1>Navbar</h1>
+        </header>
+        <main class="main">
+            @yield('content')
+        </main>
+        <footer class="footer">
+            <h1>footer</h1>
+        </footer>
+    </div>
+</body>
+</html>
